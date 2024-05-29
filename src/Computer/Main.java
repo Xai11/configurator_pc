a96791bbe183e9bad5f4ce79a1c6b4d7c3531934
Xai11 <sashakochetov300@mail.ru>
2024-05-29 12:19:59 +0300

    AChange 2

--- src/Computer/Main.java
+++ src/Computer/Main.java
@@ -10,5 +10,7 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
        Link link = new Link();
        link.Conn();
+       Computer computer = new Computer();
+       computer.createComputer();
     }
 }
