e7f004238a212006e881efcfc710e0f792447324
Ladislav-Sandalov <v.sandalov2@yandex.ru>
2024-05-30 12:17:54 +0300

    VChange +Disk

--- src/Components_computer/HardDisk.java
+++
@@ -1,9 +0,0 @@
-package Components_computer;
-
-public class HardDisk {
-    private String name;
-    private int countMemory;
-    public void newCreateHardDisk(){
-        System.out.println("Создан жесткий диск");
-    }
-}
--- src/Confugurator/IComponent.java
+++ src/Confugurator/IComponent.java
@@ -4,7 +4,7 @@ import java.io.FileNotFoundException;
 
 public interface IComponent {
     public void createBlockPower() throws FileNotFoundException;
-    public void createHardDisk();
+    public void createHardDisk() throws FileNotFoundException;
     public void createMatherBoard() throws FileNotFoundException;
     public void createProcessor() throws FileNotFoundException;
     public void createRAM();
