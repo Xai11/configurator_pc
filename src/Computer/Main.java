14608d9a49bf9933a3089420763ae1777fc6bbcc
Ladislav-Sandalov <v.sandalov2@yandex.ru>
2024-05-30 15:31:32 +0300

    VChange +Singleton

--- src/Computer/Main.java
+++ src/Computer/Main.java
@@ -7,6 +7,7 @@ import Read_File.ReadFileVideoCard;
 import java.io.FileNotFoundException;
 
 public class Main {
+
     public static void main(String[] args) throws FileNotFoundException {
         Computer pc = new Computer();
         pc.createComputer();
