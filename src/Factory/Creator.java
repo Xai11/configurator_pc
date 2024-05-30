7018c70ec4d9c0f04dfe97836e24f1d6c2f8625c
Xai11 <sashakochetov300@mail.ru>
2024-05-30 19:02:44 +0300

    removed the double request

--- src/Factory/Creator.java
+++ src/Factory/Creator.java
@@ -7,26 +7,29 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Creator implements ICreator {
-    public int getRequest(){
+    private int count = 0;
+    public void setRequest(){
         System.out.println("Выберите, какой способ сборки компьютера вам подходит:\n" +
                 "0 - Ручной (Каждую деталь нужно выбирать самостоятельно)\n" +
                 "1 - Существующий (Вам уже подобрали готовую сборку");
         Scanner request = new Scanner(System.in);
-        System.out.print("Введите свой выбор " );
+        System.out.print("Введите свой выбор: " );
         String line = request.nextLine();
-        int count = Integer.parseInt(line);
+        count += Integer.parseInt(line);
         System.out.println("");
+
+    }
+    public int getCount(){
         return count;
     }
     public void createComputer() throws FileNotFoundException {
-        if (getRequest() == 0){
+        if (getCount() == 0){
             Manual manual = new Manual();
             manual.createManual();
         }
-        else if (getRequest() == 1){
+        else if (getCount() == 1){
             Random random = new Random();
             random.createRandom();
         }
     }
-
 }
